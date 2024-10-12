d2f037ae98b47970ae3f1181fc1239161db60327
GhaziFk <ghazi.fkiri@eaexpertise.com>
2024-10-12 14:04:42 +0100

    adding requirements

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Ghazi",
+    author_email="ghazi.fkiri@ensi-uma.tn",
+    packages=find_packages(),
+)
